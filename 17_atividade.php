42ae0ecad3b6ea189185820a1271ebf7996ff4db
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-05-04 10:48:54 -0300

    update

---
+++ 17_atividade.php
@@ -0,0 +1,6 @@
+<?php
+echo['Ola Leandro Andrade Pereira'];
+print['teste com o print'];
+$vararray = [1,2,3,4,5];
+print_r($vararray);
+?>
---
+++ 18_atividade.php
@@ -0,0 +1,13 @@
+<?php
+// 1. Atribuindo valor string
+$dado = "cinco";
+echo "Valor: " . $dado . " - Tipo: " .var_dump($dado) . "<pre>";
+
+// 2. Mudando para inteiro
+$dado = 5;
+echo "Valor: " . $dado . " - Tipo: " . var_dump($dado) . "<pre>";
+
+// 3. Mudando para float (ponto flutuante)
+$dado = 5.10;
+echo "Valor: " . $dado . " - Tipo: " . var_dump($dado) . "<pre>";
+?>
