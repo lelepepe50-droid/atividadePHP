e944364f340549b6013a67d4b2cb8d72a4482143
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-05-04 11:37:29 -0300

    Update

---
+++ 19_atividade.php
@@ -0,0 +1,8 @@
+<?php
+$Cidades => ['Campinas', ' São Bernardo do Campo', 'Ribeirão Preto', 'Sorocaba', ' Americana'];
+echo $Cidades [0];
+echo $Cidades [1];
+echo $Cidades [2];
+echo $Cidades [3];
+echo $cidades [4];
+?>
---
+++ 20_atividade.php
@@ -0,0 +1,12 @@
+<?php
+
+// Criando o array associativo com as chaves login e senha
+$usuario = [
+    "login" => "Leandro Andrade Pereira",
+    "senha" => "134567@",
+];
+
+// Exibindo o valor da chave 'login'
+echo "O login cadastrado é: " . $usuario['login'];
+
+?>
